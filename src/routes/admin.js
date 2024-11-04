8524f9c43b717a73ea6abc450f299fa2357a7348
huynambka <huynambka@gmail.com>
2024-11-04 22:22:11 +0700

    add admin panel interface

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -5,7 +5,21 @@ const passportJWT = passport.authenticate("jwt-admin", {
     failureRedirect: "/auth/login",
     session: false,
 });
-
+router.get("/statistics", passportJWT, (req, res) => {
+    res.render("admin/statistics");
+});
+router.get("/users", passportJWT, (req, res) => {
+    res.render("admin/users");
+});
+router.get("/companies", passportJWT, (req, res) => {
+    res.render("admin/companies");
+});
+router.get("/registrations", passportJWT, (req, res) => {
+    res.render("admin/registrations");
+});
+router.get("/feedbacks", passportJWT, (req, res) => {
+    res.render("admin/feedbacks");
+});
 router.get("/", passportJWT, (req, res) => {
     res.render("admin/index");
 });
