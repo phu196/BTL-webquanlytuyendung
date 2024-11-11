856462c99b69290b8bfafb43b8fa3d2e9a7ba1ee
huynambka <huynambka@gmail.com>
2024-11-11 19:25:56 +0700

    update user controllers and routes

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -3,17 +3,16 @@ const mongoose = require("mongoose");
 const { StatusCodes } = require("http-status-codes");
 
 // Lấy thông tin người dùng
-// Lấy thông tin người dùng
 const getUserInfo = async (req, res) => {
     try {
         // Kiểm tra ID người dùng từ token
-        const userId = req.user ? req.user.id : req.params.id; // Sử dụng req.params.id nếu không có req.user
+        const userId = req.params.id; // Sử dụng req.params.id nếu không có req.user
 
         if (!userId) {
             return res.status(StatusCodes.BAD_REQUEST).json({ message: "User ID is not provided" });
         }
 
-        const user = await User.findById(userId);
+        const user = await User.findById(userId).select("-password");
         if (!user) {
             return res.status(StatusCodes.NOT_FOUND).json({ message: "User not found" });
         }
@@ -26,34 +25,37 @@ const getUserInfo = async (req, res) => {
 const updateUser = async (req, res) => {
     try {
         // Lấy userId từ req.body hoặc req.params (nếu không có xác thực JWT)
-        const userId = req.body.id || req.params.id;
+        if (req.user) {
+            const userId = req.user._id;
+            if (!userId) {
+                return res.status(StatusCodes.BAD_REQUEST).json({ message: "User ID is not provided" });
+            }
 
-        if (!userId) {
-            return res.status(StatusCodes.BAD_REQUEST).json({ message: "User ID is not provided" });
+            // Kiểm tra và lọc các trường hợp được phép cập nhật
+            const allowedFields = ["fullname", "gender", "phoneNumber", "address"];
+            const updateData = Object.keys(req.body)
+                .filter((key) => allowedFields.includes(key))
+                .reduce((obj, key) => {
+                    obj[key] = req.body[key];
+                    return obj;
+                }, {});
+
+            // Cập nhật người dùng trong cơ sở dữ liệu
+            const updatedUser = await User.findByIdAndUpdate(userId, updateData, { new: true, runValidators: true });
+
+            if (!updatedUser) {
+                return res.status(StatusCodes.NOT_FOUND).json({ message: "User not found" });
+            }
+
+            const { password, refreshToken, ...safeUser } = updatedUser._doc;
+
+            res.status(StatusCodes.OK).json({
+                message: "User updated successfully",
+                user: safeUser,
+            });
+        } else {
+            res.status(StatusCodes.UNAUTHORIZED).json({ message: "Unauthorized" });
         }
-
-        // Kiểm tra và lọc các trường hợp được phép cập nhật
-        const allowedFields = ["fullname", "gender", "phoneNumber", "address"];
-        const updateData = Object.keys(req.body)
-            .filter((key) => allowedFields.includes(key))
-            .reduce((obj, key) => {
-                obj[key] = req.body[key];
-                return obj;
-            }, {});
-
-        // Cập nhật người dùng trong cơ sở dữ liệu
-        const updatedUser = await User.findByIdAndUpdate(userId, updateData, { new: true, runValidators: true });
-
-        if (!updatedUser) {
-            return res.status(StatusCodes.NOT_FOUND).json({ message: "User not found" });
-        }
-
-        const { password, refreshToken, ...safeUser } = updatedUser._doc;
-
-        res.status(StatusCodes.OK).json({
-            message: "User updated successfully",
-            user: safeUser,
-        });
     } catch (error) {
         res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
             message: "Error updating user information",
