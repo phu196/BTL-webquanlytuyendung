b62ec3099ba44513e78d69c5fdb58b6e91d2f3c2
phu196 <phu.nm225375@sis.hust.edu.vn>
2024-11-25 22:19:58 +0700

    update nút apply và xem

--- src/controllers/job.js
+++ src/controllers/job.js
@@ -1,5 +1,7 @@
-const Company = require("../models/Company");
 const Job = require("../models/Job");
+const User = require("../models/User");
+const Company = require("../models/Company");
+const { search } = require("../routes/auth");
 
 const searchJob = async (req, res) => {
     const keyword = req.query.q || ""; // Nhận từ khóa từ URL
@@ -23,7 +25,7 @@ const searchJob = async (req, res) => {
 
 const showJob = async (req, res) => {
     try {
-        const jobId = req.params.id;
+        const jobId = req.params.jobId;
 
         const job = await Job.findById(jobId).populate({
             path: "company_id",
@@ -40,4 +42,32 @@ const showJob = async (req, res) => {
         res.status(500).json({ message: "This id is not exist" });
     }
 };
-module.exports = { searchJob, showJob };
+
+const applyJob = async (req, res) => {
+    try {
+        const jobId = req.params.jobId; // Lấy id công việc từ URL
+        if (!req.user) {
+            res.status(401).json({ success: false, message: "Unauthorized" });
+        }
+
+        const { cv } = req.body; // Lấy thông tin người ứng tuyển từ body
+
+        const job = await Job.findById(jobId); // Tìm công việc theo id
+        if (!job) {
+            return res.status(404).json({ success: false, message: "Job not found" });
+        }
+        const user = await User.findById(req.user._id).select("-password"); // Tìm người dùng theo id
+        if (!user) {
+            return res.status(404).json({ success: false, message: "User not found" });
+        }
+        const applicant = { userId: user._id, CV: cv }; // Tạo thông tin người ứng tuyển
+        job.applicants.push(applicant); // Thêm người ứng tuyển vào danh sách ứng tuyển
+        await job.save(); // Lưu công việc
+        user.jobs.push(jobId); // Thêm công việc vào danh sách công việc đã ứng tuyển
+        await user.save(); // Lưu người dùng
+        res.status(200).json({ success: true, message: "Applied successfully" });
+    } catch (err) {
+        return res.status(500).json({ success: false, message: "Error" });
+    }
+};
+module.exports = { searchJob, showJob, applyJob };
--- src/routes/job.js
+++ src/routes/job.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const router = express.Router();
+const passport = require("passport");
 
 const jobController = require("../controllers/job");
 
+const passportJWT = passport.authenticate("jwt", {
+    failureRedirect: "/auth/login",
+    session: false,
+});
+
 router.get("/viec-lam-it", jobController.searchJob);
 
-router.get("/:id", jobController.showJob);
+router.get("/:jobId", jobController.showJob);
+
+router.get("/:jobId/apply", passportJWT, jobController.applyJob);
+
+router.post("/:jobId/apply", passportJWT, jobController.applyJob);
 
 module.exports = router;
